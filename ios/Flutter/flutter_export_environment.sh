b5477ae79e2216510b70cf02dbd7e44d667bd421
Ömer <Bayrak>
2020-05-07 14:01:51 -0700

    ios building

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\gameg\Software\victume\mobile"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/omerbayrak/Documents/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/omerbayrak/Documents/Development/projects/victume-mobile"
+export "FLUTTER_TARGET=/Users/omerbayrak/Documents/Development/projects/victume-mobile/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/omerbayrak/Documents/Development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=2"
+export "TRACK_WIDGET_CREATION=true"
